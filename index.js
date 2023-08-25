628c8b31711115fe1cb19a47cab70fafd589f362
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-08-25 11:11:12 +0600

    html page serving operation done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express=require('express');
+const path=require('path')
 
+const app=express()
+app.listen(3000,()=>console.log(`Server listening.`))
+app.get('/',(req,res)=>{
+    res.send("this is the server")
+})
+
+app.get('/html_page',(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname,'./','views','index.html'))
+})
